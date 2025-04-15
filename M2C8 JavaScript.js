370edfa6c65b446f189c036ad0bb70b1b01290db
Fernando <garri.jf@gmail.com>
2025-04-15 09:51:15 +0200

    Add files via upload

---
+++ M2C8 JavaScript.js
@@ -0,0 +1,17 @@
+const miLista = ['velma', 'exploradora', 'jane', 'john', 'harry'];
+let i = 0;
+while (i < miLista.length) {
+    console.log(`Elemento en el índice ${i}: ${miLista[i]}`);
+    i++;
+}
+
+for (let i = 0; i < miLista.length; i++) {
+    console.log(`Elemento en el índice ${i}: ${miLista[i]}`);
+}
+
+console.log("________________________________________________");
+
+const saludar = () => {
+    return "Hola mundo";
+  };
+  console.log(saludar());
